342c770e56f7b9bae87307fa49c97f3fa3cd089c
Kirill <kirbo06@mail.ru>
2024-10-29 17:33:45 +0100

    - fixed #2 - refactoring - removed icon.ts - refactored icon setting code

--- src/ui/ChatView.ts
+++ src/ui/ChatView.ts
@@ -1,4 +1,5 @@
-import { WorkspaceLeaf, ItemView, IconName } from "obsidian";
+import { WorkspaceLeaf, ItemView, IconName, addIcon, setIcon } from "obsidian";
+import { compileFunction } from "vm";
 
 const VIEW_TYPE_CHAT = "chat-view";
 
@@ -45,81 +46,11 @@ export class ChatView extends ItemView {
 			this.messages.forEach((msg) => {
 				const message = messageArea.createDiv({cls: 'intvault-message'});
 				message.addClass(msg.sender === "user" ? "sender-user" : "sender-bot");
+				
 				if(msg.sender === "user"){
-					const userIcon = message.createSvg("svg", {
-						attr: {
-							xmlns: "http://www.w3.org/2000/svg",
-							width: "24",
-							height: "24",
-							viewBox: "0 0 24 24",
-							fill: "none",
-							stroke: "currentColor",
-							"stroke-width": "2",
-							"stroke-linecap": "round",
-							"stroke-linejoin": "round"
-						}
-					});
-					userIcon.createSvg("circle", {
-						attr: {
-							cx: "12",
-							cy:"8",
-							r:"5"
-						}
-					})
-					userIcon.createSvg("path", {
-						attr: {
-							d: "M20 21a8 8 0 0 0-16 0"
-						}
-					});
+					setIcon(message, "user-round")
 				} else if (msg.sender === "bot") {
-					const botIcon = message.createSvg("svg", {
-						attr: {
-							xmlns: "http://www.w3.org/2000/svg",
-							width: "24",
-							height: "24",
-							viewBox: "0 0 24 24",
-							fill: "none",
-							stroke: "currentColor",
-							"stroke-width": "2",
-							"stroke-linecap": "round",
-							"stroke-linejoin": "round"
-						}
-					});
-					botIcon.createSvg("path", {
-						attr: {
-							d: "M12 8V4H8"
-						}
-					})
-					botIcon.createSvg("rect", {
-						attr: {
-							width:"16",
-							height:"12",
-							x:"4",
-							y:"8",
-							rx:"2"
-						}
-					});
-					botIcon.createSvg("path", {
-						attr: {
-							d: "M2 14h2"
-						}
-					})
-					botIcon.createSvg("path", {
-						attr: {
-							d: "M20 14h2"
-						}
-					})
-					botIcon.createSvg("path", {
-						attr: {
-							d: "M15 13v2"
-						}
-					})
-					botIcon.createSvg("path", {
-						attr: {
-							d: "M9 13v2"
-						}
-					})
-					
+					setIcon(message, "bot");
 				}
 				const text = message.createDiv({cls: "intvault-message-content"});
 				text.setText(`${msg.text}`);
@@ -130,45 +61,22 @@ export class ChatView extends ItemView {
 			this.messages.push({ sender, text });
 			updateMessages();
 		};
+		const textAreaContainer = inputArea.createDiv({cls: "intvault-textarea-container"})
 
-		const input = inputArea.createEl("textarea", {type: "text", placeholder: "Prompt", cls: "intvault-chat-input" });
+		const input = inputArea.createEl("textarea", {type: "text", placeholder: "Prompt", cls: "intvault-chat-input", attr: {rows: 1}});
 		input.addEventListener('input', () => {
 			input.style.height = 'auto';
 			input.style.height = `${input.scrollHeight}px`;
-			if (input.scrollHeight > 240) {
-				input.style.height = '240px'; // 240 - это максимум для 10 строк
-				input.style.overflowY = 'auto'; // Включаем прокрутку, если текст превышает максимум
+			
+			if (input.scrollHeight > input.clientHeight) {
+				input.style.overflowY = "auto";
 			} else {
-				input.style.overflowY = 'hidden'; // Убираем прокрутку, если текст в пределах максимума
+				input.style.overflowY = 'hidden';
 			}
 		});
 		const sendButton = inputArea.createEl("button", {cls: "intvault-send-button"});
         
-        const svg = sendButton.createSvg("svg", {
-            attr: {
-                xmlns: "http://www.w3.org/2000/svg",
-                width: "24",
-                height: "24",
-                viewBox: "0 0 24 24",
-                fill: "none",
-                stroke: "currentColor",
-                "stroke-width": "2",
-                "stroke-linecap": "round",
-                "stroke-linejoin": "round"
-            }
-        });
-        
-        svg.createSvg("path", {
-            attr: {
-                d: "M3.714 3.048a.498.498 0 0 0-.683.627l2.843 7.627a2 2 0 0 1 0 1.396l-2.842 7.627a.498.498 0 0 0 .682.627l18-8.5a.5.5 0 0 0 0-.904z"
-            }
-        });
-        
-        svg.createSvg("path", {
-            attr: {
-                d: "M6 12h16"
-            }
-        });
+       setIcon(sendButton, "send-horizontal");
 
         sendButton.onclick = () => {
             const userInput = input.value.trim();
