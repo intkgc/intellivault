a6f0e7429826beef240546a25cbd4d9cac379075
intbyte-100 <66622928+intbyte-100@users.noreply.github.com>
2024-11-17 00:07:34 +0300

    fix get_files_by_keywords

--- mkadb/src/database.rs
+++ mkadb/src/database.rs
@@ -61,14 +61,9 @@ impl KeywordStore {
         for entry in fs::read_dir(dir)? {
             let entry = entry?;
             let path = entry.path();
-            if path.is_file() {
-                if let Some(ext) = path.extension() {
-                    if ext == extension {
-                        if let Some(file_name) = path.to_str() {
-                            files_with_extension.push(file_name.to_string());
-                        }
-                    }
-                }
+
+            if path.is_file() && path.to_str().unwrap().ends_with(extension) {
+                files_with_extension.push(path.to_str().unwrap().to_string());
             }
         }
 
@@ -76,11 +71,14 @@ impl KeywordStore {
     }
 
     pub async fn load(&self) {
-        Self::files_with_extension("./", ".keyboard")
+        Self::files_with_extension("./", ".keyword")
             .unwrap()
             .into_iter()
+            .map(|it| it.strip_prefix("./").unwrap_or("").to_string())
+            .filter(|it| !it.is_empty())
             .for_each(|name| {
                 let keywords = self.files.clone();
+                println!("{}", name);
                 tokio::spawn(async move {
                     let mut file = File::open(name.as_str()).unwrap();
                     let mut buffer = Vec::new();
@@ -173,7 +171,11 @@ impl KeywordStore {
             )
             .unwrap_or(String::new());
 
-        string.split(";").map(|it| it.to_string()).filter(|it| !it.is_empty()).collect()
+        string
+            .split(";")
+            .map(|it| it.to_string())
+            .filter(|it| !it.is_empty())
+            .collect()
     }
 
     pub fn add_file_if_not_exist(&self, name: &str) {
@@ -209,7 +211,7 @@ impl KeywordStore {
         let mut stmt = connection.prepare(&query).unwrap();
         let file_iter = stmt
             .query_map(rusqlite::params_from_iter(ids.iter()), |row| {
-                let name: String = row.get(1)?;
+                let name: String = row.get(0)?;
                 Ok(name)
             })
             .unwrap();
@@ -220,7 +222,11 @@ impl KeywordStore {
             .collect()
     }
 
-    pub fn get_files_ids_by_keywords(&self, rules: MatcherRules, keywords: Vec<String>) -> Vec<FileID> {
+    pub fn get_files_ids_by_keywords(
+        &self,
+        rules: MatcherRules,
+        keywords: Vec<String>,
+    ) -> Vec<FileID> {
         let locked_keywords = self.files.read().unwrap();
         let keywords: Vec<_> = keywords
             .iter()
@@ -228,26 +234,28 @@ impl KeywordStore {
             .filter(|it| it.is_some())
             .map(|it| unsafe { it.unwrap_unchecked().clone() })
             .collect();
-        Matcher::new(rules, keywords).find_matches()
+        
+         let a = Matcher::new(rules, keywords).find_matches();
+         println!("{:?}", a);
+         a
     }
 
     pub fn remove_keywords(&self, id: FileID, keywords: &Vec<String>) {
         keywords.iter().for_each(|it| {
-            
             let hash_map = self.files.read().unwrap();
             let keyword_vec = hash_map.get(it).unwrap();
             let position = keyword_vec.read().unwrap().binary_search(&id).unwrap();
-            
+
             keyword_vec.write().unwrap().remove(position);
         });
     }
-    
+
     pub fn remove_file(&self, name: &str) {
         let id = self.get_file_id(name).unwrap();
         let keywords = self.get_file_keywords(id);
-        
+
         self.remove_keywords(id, &keywords);
-        
+
         self.db
             .lock()
             .unwrap()
